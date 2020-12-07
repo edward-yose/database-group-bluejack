7c8a1741ecf598ffc6e1983c621d944a3a65cc4e
edward-yose <55052140+edward-yose@users.noreply.github.com>
2020-12-07 18:34:49 +0700

    Add files via upload
    uploaded for new D sql

--- D - Simulate_into_transaction_process.sql
+++ D - Simulate_into_transaction_process.sql
@@ -2,12 +2,30 @@
 -- Create query using DML syntax to simulate the transactions process for sales transactions and review transactions.
 -- Note: DML syntax to fill database and DML syntax to simulate the transactions process should be a different query.
 
+DECLARE 
+	@InputUserID AS CHAR(6), 
+	@InputTransactionID AS CHAR(6),
+	@InputTransactionDate AS Date,
+	@InputMovieID AS CHAR(6),
+	@InputQuantity AS INTEGER
+
+INSERT INTO (@InputUserID)
+VALUES ('USR001'
+
+INSERT INTO SalesToMovie (SalesTransactionID, MovieID, Quantity)
+VALUES ('', '', '');
+
 
 GO
-CREATE PROCEDURE [simulate transaction process]
+CREATE PROCEDURE [simulate transaction process2]
 AS
-	SELECT * FROM Review
 	SELECT * FROM SalesTransaction
+	SELECT * FROM Review
 GO;
 
-EXEC [simulate transaction process]
+--data yang terlibat
+SELECT * FROM SalesToMovie
+SELECT * FROM SalesTransaction
+SELECT * FROM Review
+
+EXEC [simulate transaction process2]
